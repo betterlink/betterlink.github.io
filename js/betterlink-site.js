6bb7a3f5ce3fcec0009710fcc179b029f4605b2f
betson <ben.getson@gmail.com>
2013-10-13 16:30:04 -0400

    re-enable linking directly to FAQ answers
    We want to enable this so that we can later highlight content and link
    directly within a FAQ answer.
    
    If either of the default code sample links are selected, clear out the
    URL's hash.

--- js/betterlink-site.js
+++ js/betterlink-site.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
-	$("div.info-bar a").click(function(e) {
-		e.preventDefault();
+	var hash = window.location.hash;
+	hash && $('.info-bar a[href="' + hash + '"]').tab('show');
+
+	$(".info-bar a").click(function(e) {
 		$(this).tab('show');
+		clearHashForDefaultView(e);
 	});
 });
+
+function clearHashForDefaultView(e) {
+	if(anchorIsForDefaultView($(e.target))) {
+		e.preventDefault();
+		window.location.hash = '';
+	}
+}
+
+function anchorIsForDefaultView(jqAnchorElement) {
+	var hash = jqAnchorElement.attr('href');
+	return hash === '#placement-example' || hash === '#script-sample';
+}
